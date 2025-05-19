6e71a73a4e312dfb7b1ea69a71e53fb91490b3e6
Alexvo1976 <106778131+Alexvo1976@users.noreply.github.com>
2025-05-18 23:58:12 -0500

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -16,7 +16,10 @@ class CarMake(models.Model):
 
 
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
+    car_make = models.ForeignKey(
+        CarMake, 
+        on_delete=models.CASCADE
+    )  # Many-to-One relationship
     name = models.CharField(max_length=100)
     CAR_TYPES = [
         ('SEDAN', 'Sedan'),
@@ -25,8 +28,8 @@ class CarModel(models.Model):
         # Add more choices as required
     ]
     type = models.CharField(
-        max_length=10, 
-        choices=CAR_TYPES, 
+        max_length=10,
+        choices=CAR_TYPES,
         default='SUV'
     )
     year = models.IntegerField(
